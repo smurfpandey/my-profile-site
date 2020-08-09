f0b0473c807b7edcb2aeccde4d3a3307418defb9
Neeraj Verma <hello@smurfpandey.me>
2020-08-09 21:29:07 +0530

    try hero text font size on netlify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   purge: [],
   theme: {
+    fontFamily: {
+      hero: ['Antique Cherry']
+    },
+    fontSize: {
+      huge: '10rem'
+    },
     extend: {},
   },
   variants: {},
