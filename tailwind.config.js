baf70e8c5633cb549d88980be9d194c625038575
Neeraj Verma <hello@smurfpandey.me>
2020-08-11 23:45:30 +0530

    added link to email

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './index.html'
+  ],
   theme: {
     fontFamily: {
-      hero: ['Antique-Cherry']
+      hero: ['Antique-Cherry'],
+      heebo: ['Heebo'],
     },
     fontSize: {
-      huge: '10rem'
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      'huge': '10rem',
     },
     extend: {},
   },
